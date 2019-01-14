d8b57def5d3128eeb68b56a8377de64e84f0b2ef
levvim <mangaril@lilac.hpc.private>
2019-01-14 15:43:20 -0500

    first working ec2 version with filtering

--- pipelines/mut_2c/scripts/filtering_aws.sh
+++ pipelines/mut_2c/scripts/filtering_aws.sh
@@ -1,26 +1,21 @@
 #!/usr/bin/bash
-#bash-4.2$ ~/lvmp/lvmp/pipelines/mut_2c/scripts/filtering.sh /data/wolchok/PROJECT/2ctest_L6
-
-#module add singularity
-#module add python/3.6.4
-#module add java
+#bash-4.2$ ~/lvmp/lvmp/pipelines/mut_2c/scripts/filtering_aws.sh
 
 ################################################################################
 # Project Config
 # input arguments
-FILE=$1
-SAMPLE=$2
-bam_readcount=/data/wolchok/singularity/bam-readcount-0.7.4.simg
-reference_genome=/data/wolchok/PROJECT/refs/GRCh37hg19/human_g1k_v37_decoy.fasta
-filtermutations=/home/mangaril/lvmp/lvmp/pipelines/mut_2c/scripts/filter_mutations.py
-snpeff_path=/data/wolchok/singularity/snpeff-4.3t.simg
+#FILE=$1
+#SAMPLE=$2
+#bam_readcount=/data/wolchok/singularity/bam-readcount-0.7.4.simg
+#reference_genome=/data/wolchok/PROJECT/refs/GRCh37hg19/human_g1k_v37_decoy.fasta
+#filtermutations=/home/mangaril/lvmp/lvmp/pipelines/mut_2c/scripts/filter_mutations.py
+#snpeff_path=/data/wolchok/singularity/snpeff-4.3t.simg
 
-
-## merge MuTect output
+## merge MuTect output 
 #cat $mutect_results_folder/mutect_stats*.txt | grep -v -e "#" -e "judgement" > $mutect_results_folder/mutect_stats.tmp
 
 #getopts for argument input
-usage() { echo -en "LVMP v1.1 EC2 setup \nExample: lvmp_ec2 \n\t-u ec2-user \n\t-m "mut_2c_metadata.csv" \n\t-s "mut_2c_samples.csv" \n\t-c "SAMPLE1" \n\nFlags:\n" && grep " .)\ #" $0; exit 0; } 
+usage() { echo -en "LVMP v1.1 EC2 setup \nExample: lvmp_ec2 \n\t-p /home/ec2-user/PROJECT1/ \n\t-s Sample1 \n\t-b /home/ec2-user/CONTAINER/bam-readcount.simg \n\t-r /home/ec2-user/refs/ucsc.hg19.fasta \n\t-f /home/ec2-user/scripts/filter_mutations.py \n\t-e /home/ec2-user/CONTAINER/snpeff-4.3t.simg\n\nFlags:\n" && grep " .)\ #" $0; exit 0; } 
 [ $# -eq 0 ] && usage
 #while getopts ":h:k:s:c:" arg; do
 while getopts ":h:p:s:b:r:f:e:" arg; do
@@ -52,28 +47,19 @@ done
 
 SAMPLE=${SAMPLE%%.*};
 SAMPLE=${SAMPLE##*/}
+echo $SAMPLE
 ################################################################################
-# This script merged the MuTect output and creates region files for bam-readcount.
-
-#mv $mutect_results_folder/mutect_stats.tmp $mutect_results_folder/mutect_stats.txt
-#cp $mutect_results_folder/mutect_stats.txt $filtering_path/
-#mutect_results=$filtering_path/mutect_stats.txt
-
-################################################################################
- #create mutect regions 
-singularity exec $FILE:$FILE $snpeff bash -c "
-cd $FILE/muTect/
+#create mutect regions 
+cd "$FILE"/muTect/
 for i in "$SAMPLE".T.call_stats.txt; do echo $i; ifix=${i%%.*};ifix=${ifix##*/};
     cp $i "$FILE"/vcf/"$ifix".T.call_stats.txt
     cat "$i" | grep -v -e "#" -e "judgement" |\
         grep -v -e "#" -e "GL0" -e "NC_007605" -e "hs37d5" | \
-        awk -v OFS='\t' '{ print $1, $2, $2 }' > "$FILE"/vcf/"$ifix".mutect_regions.txt
-    sed -i '1d' "$FILE"/vcf/"$ifix".mutect_regions.txt
+        awk -v OFS="\t" '{ print $1, $2, $2 }' > "$FILE"/vcf/"$ifix".mutect_regions.txt
+    sed -i "1d" "$FILE"/vcf/"$ifix".mutect_regions.txt
 done
-"
 
 # rename and organize strelka output into vcf folder
-singularity exec $FILE:$FILE $snpeff bash -c "
 cd $FILE/strelka/
 for i in "$SAMPLE".T; do echo $i; ifix=${i%%.*}; echo $ifix; 
     cd "$FILE"/strelka/"$i"/results; 
@@ -81,26 +67,22 @@ for i in "$SAMPLE".T; do echo $i; ifix=${i%%.*}; echo $ifix;
         cp "$ii" "$FILE"/vcf/"$ifix"."$ii"
     done
 done
-"
 
 # create strelka regions
-singularity exec $FILE:$FILE $snpeff bash -c "
 cd $FILE/vcf/
 for i in "$SAMPLE".passed.somatic.snvs.vcf; do echo $i; ifix=${i%%.*}; ifix=${ifix##*/};
     cat $i | grep -v -e "#" -e "GL0" -e "NC_007605" -e "hs37d5" | \
         awk -v OFS='\t' '{ print $1, $2, $2 }' > "$FILE"/vcf/"$ifix".strelka_regions.txt
 done
-"
 
 # cat strelka and mutect regions together
-singularity exec $FILE:$FILE $snpeff bash -c "
 cd $FILE/vcf/
 for i in "$SAMPLE".mutect_regions.txt; do echo $i; ifix=${i%%.*}; ifix=${ifix##*/};
     cat "$FILE"/vcf/"$ifix".mutect_regions.txt  "$FILE"/vcf/"$ifix".strelka_regions.txt | \
         sort -k 1 -n | uniq > "$FILE"/vcf/"$ifix".merged_regions.txt
 done
-"
-################################################################################
+
+#################################################################################
 ##calculate bam readcounts
 cd $FILE/
 for i in vcf/"$SAMPLE".merged_regions.txt; do echo $i; ifix=${i%%.*}; ifix=${ifix##*/};
@@ -121,35 +103,33 @@ for i in vcf/"$SAMPLE".merged_regions.txt; do echo $i; ifix=${i%%.*}; ifix=${ifi
 done
 
 #################################################################################
-# organize vcfs
-# delete header lines from vcfs
-singularity exec $FILE:$FILE $snpeff bash -c "
+ organize vcfs
+ delete header lines from vcfs
 cd $FILE/vcf/
 for i in "$SAMPLE".mutect_regions.txt; do echo $i; ifix=${i%%.*}; ifix=${ifix##*/};
-    sed -i -e '1,115d' "$ifix".passed.somatic.snvs.vcf
-    sed -i -e '1,2d' "$ifix".T.call_stats.txt
+    sed -i -e "1,115d" "$ifix".passed.somatic.snvs.vcf
+    sed -i -e "1,2d" "$ifix".T.call_stats.txt
 done
-"
 
-singularity exec $FILE:$FILE $snpeff bash -c "
 cd $FILE/vcf
 for i in "$SAMPLE".merged_regions.txt; do echo $i; ifix=${i%%.*}; ifix=${ifix##*/};
-    python3 $filtermutations \
-        "$ifix" \
-        "$ifix".T.call_stats.txt \
-        "$ifix".passed.somatic.snvs.vcf \
-        "$ifix".normal_bam_readcount.txt \
-        "$ifix".tumor_bam_readcount.txt \
-        $FILE/vcf/
+	singularity exec --bind $FILE:$FILE $snpeff bash -c "
+	    python3 \"$filtermutations\" \
+	        \"$ifix\" \
+	        \"$ifix\".T.call_stats.txt \
+	        \"$ifix\".passed.somatic.snvs.vcf \
+	        \"$ifix\".normal_bam_readcount.txt \
+	        \"$ifix\".tumor_bam_readcount.txt \
+	        \"$FILE\"/vcf/;"
 done
-"
+
         
-#################################################################################
+##################################################################################
 #run snpeff
-singularity exec $FILE:$FILE $snpeff bash -c "
 cd $FILE/vcf
-for i in "$SAMPLE".merged_regions.txt; do echo $i; ifix=${i%%.*}; ifix=${ifix##*/};
-    java -jar $snpeff_path ann \
+for i in "$SAMPLE".merged_regions.txt; do echo $i; ifix="${i%%.*}"; ifix="${ifix##*/}";
+singularity exec --bind $FILE:$FILE $snpeff bash -c "
+    java -jar /snpEff/snpEff.jar ann \
         -noStats \
         -strict \
         -no-downstream \
@@ -162,100 +142,19 @@ for i in "$SAMPLE".merged_regions.txt; do echo $i; ifix=${i%%.*}; ifix=${ifix##*
         -canon \
         -v \
         -onlyProtein \
-        -fastaProt "$FILE"/vcf/"$fix".ann.fasta \
+        -fastaProt \"$FILE\"/vcf/\"$fix\".ann.fasta \
         GRCh37.75 \
-        "$ifix".vcf > "$ifix".ann.vcf
+        \"$ifix\".vcf > \"$ifix\".ann.vcf;"
 done
-"
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+################################################################################
